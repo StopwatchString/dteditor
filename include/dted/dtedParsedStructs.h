abae2e0ceacc441e0583d061ef9bdce87d9e8e53
Mason Speck <mcs.5299@gmail.com>
2025-01-03 22:38:16 -0500

    swapped to std::from_chars

--- include/dted/dtedParsedStructs.h
+++ include/dted/dtedParsedStructs.h
@@ -30,43 +30,43 @@ struct UserHeaderLabel
 
 struct DataSetIdentification
 {
-    std::string recognitionSentinel;         // 1-3     | Recognition sentinel
-    char securityCode;                       // 4       | Unclassified security code
-    std::string securityControlMarkings;     // 5-6     | Security control markings
-    std::string securityHandlingDescription; // 7-33    | Security handling description
-    uint8_t dtedLevel;                       // 60-64   | DTED level designator
-    std::string uniqueReference;             // 65-79   | Unique reference number
-    uint8_t dataEditionNumber;               // 88-89   | Data edition number
-    char matchMergeVersion;                  // 90      | Match/merge version
-    std::chrono::year_month maintenanceDate; // 91-94   | Maintenance date
-    std::chrono::year_month matchMergeDate;  // 95-98   | Match/merge date
-    uint8_t maintenanceDescCode;             // 99-102  | Maintenance description code
-    std::string producerCode;                // 103-110 | Producer code
-    std::string productSpec;                 // 127-135 | Product specification
-    uint8_t productSpecAmendmentNumber;      // 136-137 | Product spec amendment number
-    std::chrono::year_month productSpecDate; // 138-141 | Date of product specification
-    std::string verticalDatum;               // 142-144 | Vertical datum
-    std::string horizontalDatum;             // 145-149 | Horizontal datum
-    std::string digitizingSystem;            // 150-159 | Digitizing/collection system
-    std::chrono::year_month compilationDate; // 160-163 | Compilation date
-    double latitudeOfOriginDeg;              // 186-194 | Latitude of origin
-    double longitudeOfOriginDeg;             // 195-204 | Longitude of origin
-    double latSWCornerDeg;                   // 205-211 | Latitude of SW corner
-    double lonSWCornerDeg;                   // 212-219 | Longitude of SW corner
-    double latNWCornerDeg;                   // 220-226 | Latitude of NW corner
-    double lonNWCornerDeg;                   // 227-234 | Longitude of NW corner
-    double latNECornerDeg;                   // 235-241 | Latitude of NE corner
-    double lonNECornerDeg;                   // 242-249 | Longitude of NE corner
-    double latSECornerDeg;                   // 250-256 | Latitude of SE corner
-    double lonSECornerDeg;                   // 257-264 | Longitude of SE corner
-    double orientationAngle;                 // 265-273 | Clockwise orientation angle
-    double latitudeIntervalArcSeconds;       // 274-277 | Latitude interval
-    double longitudeIntervalArcSeconds;      // 278-281 | Longitude interval
-    uint16_t numberLatitudeLines;            // 282-285 | Number of latitude lines
-    uint16_t numberLongitudeLines;           // 286-289 | Number of longitude lines
-    uint8_t partialCellIndicator;            // 290-291 | Partial cell indicator
-    double coveragePercent;                  // 292-392 | Coverage percentage
-    std::string geoidUndulation;             // 393-492 | Geoid undulation
+    std::string recognitionSentinel;          // 1-3     | Recognition sentinel
+    char securityCode;                        // 4       | Unclassified security code
+    std::string securityControlMarkings;      // 5-6     | Security control markings
+    std::string securityHandlingDescription;  // 7-33    | Security handling description
+    uint8_t dtedLevel;                        // 60-64   | DTED level designator
+    std::string uniqueReference;              // 65-79   | Unique reference number
+    uint8_t dataEditionNumber;                // 88-89   | Data edition number
+    char matchMergeVersion;                   // 90      | Match/merge version
+    std::chrono::year_month maintenanceDate;  // 91-94   | Maintenance date
+    std::chrono::year_month matchMergeDate;   // 95-98   | Match/merge date
+    uint8_t maintenanceDescCode;              // 99-102  | Maintenance description code
+    std::string producerCode;                 // 103-110 | Producer code
+    std::string productSpec;                  // 127-135 | Product specification
+    uint8_t productSpecAmendmentNumber;       // 136-137 | Product spec amendment number
+    std::chrono::year_month productSpecDate;  // 138-141 | Date of product specification
+    std::string verticalDatum;                // 142-144 | Vertical datum
+    std::string horizontalDatum;              // 145-149 | Horizontal datum
+    std::string digitizingSystem;             // 150-159 | Digitizing/collection system
+    std::chrono::year_month compilationDate;  // 160-163 | Compilation date
+    double latitudeOfOriginTenthsArcSeconds;  // 186-194 | Latitude of origin
+    double longitudeOfOriginTenthsArcSeconds; // 195-204 | Longitude of origin
+    double latSWCornerTenthsArcSeconds;       // 205-211 | Latitude of SW corner
+    double lonSWCornerTenthsArcSeconds;       // 212-219 | Longitude of SW corner
+    double latNWCornerTenthsArcSeconds;       // 220-226 | Latitude of NW corner
+    double lonNWCornerTenthsArcSeconds;       // 227-234 | Longitude of NW corner
+    double latNECornerTenthsArcSeconds;       // 235-241 | Latitude of NE corner
+    double lonNECornerTenthsArcSeconds;       // 242-249 | Longitude of NE corner
+    double latSECornerTenthsArcSeconds;       // 250-256 | Latitude of SE corner
+    double lonSECornerTenthsArcSeconds;       // 257-264 | Longitude of SE corner
+    double orientationAngle;                  // 265-273 | Clockwise orientation angle
+    double latitudeIntervalArcSeconds;        // 274-277 | Latitude interval
+    double longitudeIntervalArcSeconds;       // 278-281 | Longitude interval
+    uint16_t numberLatitudeLines;             // 282-285 | Number of latitude lines
+    uint16_t numberLongitudeLines;            // 286-289 | Number of longitude lines
+    uint8_t partialCellIndicator;             // 290-291 | Partial cell indicator
+    double coveragePercent;                   // 292-392 | Coverage percentage
+    std::string geoidUndulation;              // 393-492 | Geoid undulation
 
     DataSetIdentification() = delete;
     DataSetIdentification(const DataSetIdentificationBlob& dsiBlob);
--- src/dted/dtedParsedStructs.cpp
+++ src/dted/dtedParsedStructs.cpp
@@ -3,227 +3,183 @@
 #include <string>
 #include <iostream>
 #include <concepts>
+#include <charconv>
 
 template <size_t S>
-static std::string toString(const std::array<std::byte, S>& bytes, size_t offset, size_t size) {
-    return std::string(reinterpret_cast<const char*>(bytes.data() + offset), size);
+static void stringFromChars(const std::array<std::byte, S>& bytes, std::string& target)
+{
+    target.resize(bytes.size());
+    memcpy(target.data(), bytes.data(), bytes.size());
 }
 
-template<std::integral T, size_t S>
-static T toIntegral(const std::array<std::byte, S>& bytes, size_t offset, size_t size) {
-    T out = static_cast<T>(0);
-    for (uint32_t i = 0; i < size; i++) {
-        out *= static_cast<T>(10);
-        out += static_cast<T>(bytes[i + offset]) - static_cast<T>('0');
-    }
-    return out;
+template <std::integral T, size_t S>
+static T integralFromChars(const std::array<std::byte, S>& bytes, size_t offset, size_t size, int base = 10)
+{
+    T retVal = T();
+    std::from_chars_result result = std::from_chars(
+        reinterpret_cast<const char*>(&bytes[offset]),
+        reinterpret_cast<const char*>(&bytes[offset + size - 1]),
+        retVal,
+        base
+    );
+    return T;
 }
 
-template<std::floating_point T, size_t S>
-static T toFloating(const std::array<std::byte, S>& bytes, size_t offset, size_t size, size_t decimalAfterIndex) {
-    if (decimalAfterIndex > size) {
-        std::cout << "WARNING toFloating() Decimal is set to occur outside of designated size." << std::endl;
-        decimalAfterIndex = size;
-    }
-
-    T out = static_cast<T>(0);
-    uint32_t index = 0;
-    // Handle whole number values
-    while (index < decimalAfterIndex) {
-        out *= static_cast<T>(10.0);
-        out += static_cast<T>(bytes[index + offset]) - static_cast<T>('0');
-        index++;
-    }
-
-    // Handle after the decimal place
-    int decimalIndex = offset + size - 1;
-    T decimal = static_cast<T>(0.0);
-    while (index < decimalIndex) {
-        decimal += static_cast<T>(bytes[decimalIndex + offset]) - static_cast<T>('0');
-        decimal *= static_cast<T>(0.1);
-        decimalIndex--;
-    }
-
-    return out + decimal;
-}
-
-template<std::floating_point T, size_t S>
-static T toFloatingDecimalPresent(const std::array<std::byte, S>& bytes, size_t offset, size_t size) {
-    T out = static_cast<T>(0);
-    uint32_t index = 0;
-    // Handle whole number values
-    while (index < size && bytes[index + offset] != static_cast<std::byte>('.')) {
-        out *= static_cast<T>(10.0);
-        out += static_cast<T>(bytes[index + offset]) - static_cast<T>('0');
-        index++;
-    }
-
-    // Handle after the decimal place
-    int decimalIndex = offset + size - 1;
-    T decimal = static_cast<T>(0.0);
-    while (index < decimalIndex) {
-        decimal += static_cast<T>(bytes[decimalIndex + offset]) - static_cast<T>('0');
-        decimal *= static_cast<T>(0.1);
-        decimalIndex--;
-    }
-
-    return out + decimal;
+template <std::floating_point T, size_t S>
+static T floatingPointFromChars(const std::array<std::byte, S>& bytes, size_t offset, size_t size, std::chars_format fmt = std::chars_format::general)
+{
+    T retVal = T();
+    std::from_chars_result result = std::from_chars(
+        reinterpret_cast<const char*>(&bytes[offset]),
+        reinterpret_cast<const char*>(&bytes[offset + size - 1]),
+        retVal,
+        fmt
+    );
+    return T;
 }
 
 namespace dted {
-
-
 UserHeaderLabel::UserHeaderLabel(const UserHeaderLabelBlob& uhlBlob)
 {
-    // recognitionSentinel
-    recognitionSentinel = toString(uhlBlob.recognitionSentinel, 0, 3);
+    //// recognitionSentinel
+    //stringFromChars(uhlBlob.recognitionSentinel, recognitionSentinel);
+    //
+    //// fixedbyStandard
+    //fixedByStandard = static_cast<uint8_t>(uhlBlob.fixedStandard[0]);
 
-    // fixedbyStandard
-    fixedByStandard = static_cast<uint8_t>(uhlBlob.fixedStandard[0]);
+    //// longitudeOfOrigin
+    //auto result = std::from_chars((const char*)(&uhlBlob.longitudeOfOrigin[0]), (const char*)(&uhlBlob.longitudeOfOrigin[uhlBlob.longitudeOfOrigin.size() - 1]), longitudeOfOrigin);
+    //std::cout << longitudeOfOrigin << std::endl;
+    //
+    //longitudeOfOrigin = floatingPointFromChars<double>(uhlBlob.longitudeOfOrigin, 0, 7);
+    //if (uhlBlob.longitudeOfOrigin[7] == (std::byte)'W') {
+    //    longitudeOfOrigin *= -1;
+    //}
+    //if (longitudeOfOrigin >= 180.0 || longitudeOfOrigin <= -180.0) {
+    //    std::cerr << "WARNING UserHeaderLabel() Longitude of origin has invalid range. [-180.0, 180.0]: " << longitudeOfOrigin << std::endl;
+    //}
+    //// latitudeOfOrigin
+    //latitudeOfOrigin = floatingPointFromChars<double>(uhlBlob.latitudeOfOrigin, 0, 7);
+    //if (uhlBlob.latitudeOfOrigin[7] == (std::byte)'S') {
+    //    latitudeOfOrigin *= -1;
+    //}
+    //if (latitudeOfOrigin >= 90.0 || latitudeOfOrigin <= -90.0) {
+    //    std::cerr << "WARNING UserHeaderLabel() Latitude of origin has invalid range. [-90.0, 90.0]: " << latitudeOfOrigin << std::endl;
+    //}
 
-    // longitudeOfOrigin
-    longitudeOfOrigin = toFloating<double>(uhlBlob.longitudeOfOrigin, 0, 7, 3);
-    if (uhlBlob.longitudeOfOrigin[7] == (std::byte)'W') {
-        longitudeOfOrigin *= -1;
-    }
-    if (longitudeOfOrigin >= 180.0 || longitudeOfOrigin <= -180.0) {
-        std::cerr << "WARNING UserHeaderLabel() Longitude of origin has invalid range. [-180.0, 180.0]: " << longitudeOfOrigin << std::endl;
-    }
-    // latitudeOfOrigin
-    latitudeOfOrigin = toFloating<double>(uhlBlob.latitudeOfOrigin, 0, 7, 3);
-    if (uhlBlob.latitudeOfOrigin[7] == (std::byte)'S') {
-        latitudeOfOrigin *= -1;
-    }
-    if (latitudeOfOrigin >= 90.0 || latitudeOfOrigin <= -90.0) {
-        std::cerr << "WARNING UserHeaderLabel() Latitude of origin has invalid range. [-90.0, 90.0]: " << latitudeOfOrigin << std::endl;
-    }
+    //// longitudeIntervalArcSeconds
+    //longitudeIntervalArcSeconds = integralFromChars<uint16_t>(uhlBlob.longitudeIntervalArcSeconds, 0, 4);
 
-    // longitudeIntervalArcSeconds
-    longitudeIntervalArcSeconds = toIntegral<uint16_t>(uhlBlob.longitudeIntervalArcSeconds, 0, 4);
+    //// latitudeIntervalArcSeconds
+    //latitudeIntervalArcSeconds = integralFromChars<uint16_t>(uhlBlob.latitudeIntervalArcSeconds, 0, 4);
 
-    // latitudeIntervalArcSeconds
-    latitudeIntervalArcSeconds = toIntegral<uint16_t>(uhlBlob.latitudeIntervalArcSeconds, 0, 4);
+    //// absoluteVerticalAccuracy
+    //stringFromChars(uhlBlob.absoluteVerticalAccuracy, absoluteVerticalAccuracy);
+    //std::string absoluteVerticalAccuracyString = toString(uhlBlob.absoluteVerticalAccuracy, 0, 4);
+    //if (absoluteVerticalAccuracyString != "NA$$") {
+    //    absoluteVerticalAccuracy = std::stoi(absoluteVerticalAccuracyString);
+    //}
 
-    // absoluteVerticalAccuracy
-    std::string absoluteVerticalAccuracyString = toString(uhlBlob.absoluteVerticalAccuracy, 0, 4);
-    if (absoluteVerticalAccuracyString != "NA$$") {
-        absoluteVerticalAccuracy = std::stoi(absoluteVerticalAccuracyString);
-    }
+    //// unclassifiedSecurityCode
+    //unclassifiedSecurtiyCode = static_cast<char>(uhlBlob.securityCode[0]);
 
-    // unclassifiedSecurityCode
-    unclassifiedSecurtiyCode = (reinterpret_cast<const char*>(uhlBlob.securityCode.data()))[0];
+    //// uniqueReferenceNumber
+    //stringFromChars(uhlBlob.uniqueReference, uniqueReferenceNumber);
+    //
+    //// numberOfLongitudeLines
+    //numberOfLongitudeLines = toIntegral<uint16_t>(uhlBlob.numberOfLongitudeLines, 0, 4);
 
-    // uniqueReferenceNumber
-    uniqueReferenceNumber = toString(uhlBlob.uniqueReference, 0, 12);
+    //// numberOfLatitudePoints
+    //numberOfLatitudePoints = toIntegral<uint16_t>(uhlBlob.numberOfLatitudePoints, 0, 4);
 
-    // numberOfLongitudeLines
-    numberOfLongitudeLines = toIntegral<uint16_t>(uhlBlob.numberOfLongitudeLines, 0, 4);
-
-    // numberOfLatitudePoints
-    numberOfLatitudePoints = toIntegral<uint16_t>(uhlBlob.numberOfLatitudePoints, 0, 4);
-
-    // multipleAccuracy
-    multipleAccuracy = static_cast<int>(static_cast<char>(uhlBlob.multipleAccuracy[0]) - '0') > 0;
+    //// multipleAccuracy
+    //multipleAccuracy = static_cast<int>(static_cast<char>(uhlBlob.multipleAccuracy[0]) - '0') > 0;
 }
 
 DataSetIdentification::DataSetIdentification(const DataSetIdentificationBlob& dsiBlob)
 {
-    //recognitionSentinel
-    recognitionSentinel = toString(dsiBlob.recognitionSentinel, 0, 3);
+    ////recognitionSentinel
+    //recognitionSentinel = toString(dsiBlob.recognitionSentinel, 0, 3);
 
-    //securityCode
-    securityCode = static_cast<char>(dsiBlob.securityCode[0]);
+    ////securityCode
+    //securityCode = static_cast<char>(dsiBlob.securityCode[0]);
 
-    //securityControlMarkings
-    securityControlMarkings = toString(dsiBlob.securityControlMarkings, 0, 2);
+    ////securityControlMarkings
+    //securityControlMarkings = toString(dsiBlob.securityControlMarkings, 0, 2);
 
-    //securityHandlingDescription
-    securityHandlingDescription = toString(dsiBlob.securityHandlingDescription, 0, 27);
+    ////securityHandlingDescription
+    //securityHandlingDescription = toString(dsiBlob.securityHandlingDescription, 0, 27);
 
-    //dtedLevel
-    dtedLevel = std::stoi(toString(dsiBlob.dtedLevel, 0, 2));
+    ////dtedLevel
+    //dtedLevel = toIntegral<uint8_t>(dsiBlob.dtedLevel, 4, 1);
 
-    //uniqueReference
-    uniqueReference = toString(dsiBlob.uniqueReference, 0, 15);
+    ////uniqueReference
+    //uniqueReference = toString(dsiBlob.uniqueReference, 0, 15);
 
-    //dataEditionNumber
-    dataEditionNumber = std::stoi(toString(dsiBlob.dataEditionNumber, 0, 2));
+    ////dataEditionNumber
+    //dataEditionNumber = std::stoi(toString(dsiBlob.dataEditionNumber, 0, 2));
 
-    //matchMergeVersion
-    matchMergeVersion = static_cast<char>(dsiBlob.matchMergeVersion[0]);
+    ////matchMergeVersion
+    //matchMergeVersion = static_cast<char>(dsiBlob.matchMergeVersion[0]);
 
-    //maintenanceDate
-    std::chrono::year maintenanceDateYear(std::stoi(toString(dsiBlob.maintenanceDate, 0, 2)));
-    std::chrono::month maintenanceDateMonth(std::stoi(toString(dsiBlob.maintenanceDate, 2, 2)));
-    maintenanceDate = std::chrono::year_month(maintenanceDateYear, maintenanceDateMonth);
+    ////maintenanceDate
+    //std::chrono::year maintenanceDateYear(std::stoi(toString(dsiBlob.maintenanceDate, 0, 2)));
+    //std::chrono::month maintenanceDateMonth(std::stoi(toString(dsiBlob.maintenanceDate, 2, 2)));
+    //maintenanceDate = std::chrono::year_month(maintenanceDateYear, maintenanceDateMonth);
 
-    //matchMergeDate
-    std::chrono::year matchMergeDateYear(std::stoi(toString(dsiBlob.matchMergeDate, 0, 2)));
-    std::chrono::month matchMergeDateMonth(std::stoi(toString(dsiBlob.matchMergeDate, 2, 2)));
-    maintenanceDate = std::chrono::year_month(matchMergeDateYear, matchMergeDateMonth);
+    ////matchMergeDate
+    //std::chrono::year matchMergeDateYear(std::stoi(toString(dsiBlob.matchMergeDate, 0, 2)));
+    //std::chrono::month matchMergeDateMonth(std::stoi(toString(dsiBlob.matchMergeDate, 2, 2)));
+    //maintenanceDate = std::chrono::year_month(matchMergeDateYear, matchMergeDateMonth);
 
-    //maintenanceDescCode
-    maintenanceDescCode = std::stoi(toString(dsiBlob.maintenanceDescCode, 0, 4));
+    ////maintenanceDescCode
+    //maintenanceDescCode = toIntegral<uint8_t>(dsiBlob.maintenanceDescCode, 0, 4);
 
-    //producerCode
-    producerCode = toString(dsiBlob.producerCode, 0, 8);
+    ////producerCode
+    //producerCode = toString(dsiBlob.producerCode, 0, 8);
 
-    //productSpec
-    productSpec = toString(dsiBlob.productSpec, 0, 9);
+    ////productSpec
+    //productSpec = toString(dsiBlob.productSpec, 0, 9);
 
-    //productSpecAmendmentNumber
-    productSpecAmendmentNumber = std::stoi(toString(dsiBlob.productSpecAmend, 0, 2));
+    ////productSpecAmendmentNumber
+    //productSpecAmendmentNumber = std::stoi(toString(dsiBlob.productSpecAmend, 0, 2));
 
-    //productSpecDate
-    std::chrono::year productSpecDateYear(std::stoi(toString(dsiBlob.productSpecDate, 0, 2)));
-    std::chrono::month productSpecDateMonth(std::stoi(toString(dsiBlob.productSpecDate, 2, 2)));
-    productSpecDate = std::chrono::year_month(productSpecDateYear, productSpecDateMonth);
+    ////productSpecDate
+    //std::chrono::year productSpecDateYear(toIntegral<uint8_t>(dsiBlob.productSpecDate, 0, 2));
+    //std::chrono::month productSpecDateMonth(toIntegral<uint8_t>(dsiBlob.productSpecDate, 2, 2));
+    //productSpecDate = std::chrono::year_month(productSpecDateYear, productSpecDateMonth);
 
-    //verticalDatum
-    verticalDatum = toString(dsiBlob.verticalDatum, 0, 3);
+    ////verticalDatum
+    //verticalDatum = toString(dsiBlob.verticalDatum, 0, 3);
 
-    //horizontalDatum
-    horizontalDatum = toString(dsiBlob.horizontalDatum, 0, 5);
+    ////horizontalDatum
+    //horizontalDatum = toString(dsiBlob.horizontalDatum, 0, 5);
 
-    //digitizingSystem
-    digitizingSystem = toString(dsiBlob.digitizingSystem, 0, 10);
+    ////digitizingSystem
+    //digitizingSystem = toString(dsiBlob.digitizingSystem, 0, 10);
 
-    //compilationDate
-    std::chrono::year compilationDateYear(std::stoi(toString(dsiBlob.compilationDate, 0, 2)));
-    std::chrono::month compilationDateMonth(std::stoi(toString(dsiBlob.compilationDate, 2, 2)));
-    compilationDate = std::chrono::year_month(compilationDateYear, compilationDateMonth);
+    ////compilationDate
+    //std::chrono::year compilationDateYear(toIntegral<uint8_t>(dsiBlob.compilationDate, 0, 2));
+    //std::chrono::month compilationDateMonth(toIntegral<uint8_t>(dsiBlob.compilationDate, 2, 2));
+    //compilationDate = std::chrono::year_month(compilationDateYear, compilationDateMonth);
 
-    //latitudeOfOriginDeg
-    latitudeOfOriginDeg = std::stod(
-        toString(dsiBlob.latOrigin, 0, 2)
-        + "."
-        + toString(dsiBlob.latOrigin, 2, 4)
-        + toString(dsiBlob.latOrigin, 7, 1)
-        );
-    if (dsiBlob.latOrigin[8] == static_cast<std::byte>('S')) {
-        latitudeOfOriginDeg *= -1.0;
-    }
+    ////latitudeOfOriginDeg
+    //latitudeOfOriginDeg = toFloating<double>(dsiBlob.latOrigin, 0, 8, 2);
+    //if (dsiBlob.latOrigin[8] == static_cast<std::byte>('S')) {
+    //    latitudeOfOriginDeg *= -1.0;
+    //}
 
-    //longitudeOfOriginDeg
-    longitudeOfOriginDeg = std::stod(
-        toString(dsiBlob.lonOrigin, 0, 3)
-        + "."
-        + toString(dsiBlob.lonOrigin, 3, 4)
-        + toString(dsiBlob.lonOrigin, 8, 1)
-    );
-    if (dsiBlob.lonOrigin[9] == static_cast<std::byte>('W')) {
-        longitudeOfOriginDeg *= -1.0;
-    }
+    ////longitudeOfOriginDeg
+    //longitudeOfOriginDeg = toFloatingDecimalPresent<double>(dsiBlob.lonOrigin, 0, 9);
+    //if (dsiBlob.lonOrigin[9] == static_cast<std::byte>('W')) {
+    //    longitudeOfOriginDeg *= -1.0;
+    //}
 
-    //latSWCornerDeg
-    latSWCornerDeg = std::stod(
-        toString(dsiBlob.latSWCorner, 0, 2)
-        + "."
-        + toString(dsiBlob.latSWCorner, 2, 4)
-    );
-    if (dsiBlob.latSWCorner[7] == static_cast<std::byte>('S')) {
-        latSWCornerDeg *= -1.0;
-    }
+    ////latSWCornerDeg
+    //latSWCornerDeg = toFloating<double>(dsiBlob.latSWCorner, 0, 3, 3);
+    //if (dsiBlob.latSWCorner[7] == static_cast<std::byte>('S')) {
+    //    latSWCornerDeg *= -1.0;
+    //}
 
     //lonSWCornerDeg
 
