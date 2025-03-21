e5472e7f670858e017385d04faed0b116c9b139e
Mason Speck <mcs.5299@gmail.com>
2025-03-20 20:31:27 -0400

    Added tracy debugger (missing CMakePreset settings)

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -9,13 +9,17 @@ using namespace dted;
 #include "cpputils/Timer.h"
 #include "cpputils/windows/simpleDirectIo.h"
 
+#include "tracy/Tracy.hpp"
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <filesystem>
+#include <conio.h>
 
 const std::filesystem::path file1 = "..\\data\\n39_w084_1arc_v3.dt2";
 const std::filesystem::path file2 = "..\\data\\n39_w085_1arc_v3.dt2";
+const std::filesystem::path file3 = "C:\\data\\n39_w084_1arc_v3.dt2";
 static constexpr size_t ALIGNMENT = 4096;
 static constexpr char FILL = 0;
 
@@ -46,40 +50,63 @@ size_t writeDataAligned(std::ofstream& file, const char* buf, const size_t bufSi
 //-------------------------------------------------------------------------------
 bool generateAlignedDtedFile(const std::filesystem::path& file)
 {
-    cpputils::win::AlignedBuffer fileBuffer = cpputils::win::directFileToBuffer(file);
-    if (fileBuffer.buf == nullptr) return false;
+    ZoneScoped;
 
-    std::filesystem::path newFile = file.string() + ".aligned";
-    std::ofstream outputFile(newFile, std::ios::binary);
-    if (!outputFile.is_open()) {
-        std::cerr << "ERROR Cannot open file " << newFile << std::endl;
-        return false;
+    cpputils::win::AlignedBuffer fileBuffer;
+
+    {
+        ZoneScopedN("Read File");
+        fileBuffer = cpputils::win::directFileToBuffer(file);
+        if (fileBuffer.buf == nullptr) return false;
     }
 
-    uint32_t headerSize = USER_HEADER_LABEL_BLOB_SIZE + DATA_SET_IDENTIFICATION_BLOB_SIZE + ACCURACY_DESCRIPTION_RECORD_BLOB_SIZE;
-    size_t sectors = writeDataAligned(outputFile, (const char*)fileBuffer.buf, headerSize);
+    {
+        ZoneScopedN("Write File");
 
-    UserHeaderLabelBlob uhlBlob = *reinterpret_cast<UserHeaderLabelBlob*>((char*)fileBuffer.buf + USER_HEADER_LABEL_BLOB_OFFSET);
-    UserHeaderLabel uhl(uhlBlob);
+        std::filesystem::path newFile = file.string() + ".aligned";
+        std::ofstream outputFile(newFile, std::ios::binary);
+        if (!outputFile.is_open()) {
+            std::cerr << "ERROR Cannot open file " << newFile << std::endl;
+            return false;
+        }
 
-    uint32_t recordsOffset = headerSize;
-    uint32_t columnHeaderSize = COLUMN_HEADER_BLOB_SIZE;
-    uint32_t columnDataSize = sizeof(int16_t) * uhl.numberOfLatitudePoints;
-    uint32_t columnFooterSize = COLUMN_FOOTER_BLOB_SIZE;
-    uint32_t recordSize = columnHeaderSize + columnDataSize + columnFooterSize;
+        uint32_t headerSize = USER_HEADER_LABEL_BLOB_SIZE + DATA_SET_IDENTIFICATION_BLOB_SIZE + ACCURACY_DESCRIPTION_RECORD_BLOB_SIZE;
+        size_t sectors = writeDataAligned(outputFile, (const char*)fileBuffer.buf, headerSize);
 
-    for (size_t lon = 0; lon < uhl.numberOfLongitudeLines; lon++) {
-        uint32_t baseOffset = recordsOffset + lon * recordSize;
-        writeDataAligned(outputFile, (const char*)fileBuffer.buf + baseOffset, recordSize);
+        UserHeaderLabelBlob uhlBlob = *reinterpret_cast<UserHeaderLabelBlob*>((char*)fileBuffer.buf + USER_HEADER_LABEL_BLOB_OFFSET);
+        UserHeaderLabel uhl(uhlBlob);
+
+        uint32_t recordsOffset = headerSize;
+        uint32_t columnHeaderSize = COLUMN_HEADER_BLOB_SIZE;
+        uint32_t columnDataSize = sizeof(int16_t) * uhl.numberOfLatitudePoints;
+        uint32_t columnFooterSize = COLUMN_FOOTER_BLOB_SIZE;
+        uint32_t recordSize = columnHeaderSize + columnDataSize + columnFooterSize;
+
+        for (size_t lon = 0; lon < uhl.numberOfLongitudeLines; lon++) {
+            uint32_t baseOffset = recordsOffset + lon * recordSize;
+            writeDataAligned(outputFile, (const char*)fileBuffer.buf + baseOffset, recordSize);
+        }
+
+        outputFile.close();
     }
-
-    outputFile.close();
     return false;
 }
 
 int main()
 {
-    generateAlignedDtedFile(file1);
+    bool running = true;
+    while (running) {
+        generateAlignedDtedFile(file3);
+
+        if (_kbhit()) {
+            int ch = std::tolower(_getch());
+            if (ch == 'q') {
+                running = false;
+            }
+        }
+
+        FrameMark;
+    }
 
     return EXIT_SUCCESS;
 }
